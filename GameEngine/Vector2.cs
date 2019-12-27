9e13adcd0cb441640c7a5deb1aca4e3abef0b0e9
Thomas the Franque <tfranque@gmail.com>
2019-12-27 17:52:27 +0000

    Start of GameEngine

---
+++ GameEngine/Vector2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine
+{
+	/// <summary>
+	/// Struct stores the position on screen
+	/// </summary>
+	public struct Vector2
+	{
+		/// <summary>
+		/// Right screen position.
+		/// </summary>
+		public uint y { get; }
+		/// <summary>
+		/// Top Screen position.
+		/// </summary>
+		public uint x { get; }
+
+		/// <summary>
+		/// Vector 2 constructor
+		/// </summary>
+		/// <param name="x">Initial X position.</param>
+		/// <param name="y">Initial Y position.</param>
+		public Vector2(uint x, uint y)
+		{
+			this.x = x;
+			this.y = y;
+		}
+	}
+}
---
+++ GameEngine/ZetrisPiece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameEngine
+{
+	class ZetrisPiece
+	{
+		public Vector2 position { get; }
+		public uint rotation { get; }
+		public string shape { get; }
+
+		public ZetrisPiece(string shape)
+		{
+			this.shape = shape;
+		}
+	}
+}
